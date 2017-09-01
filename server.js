a71607826a34cd9297ba93e691d9c20b4959efd3
helmut cardenas <aleksey89col@gmail.com>
2017-08-31 20:56:06 -0400

    final commit

--- server.js
+++ server.js
@@ -23,48 +23,49 @@ var waitlist = [];
 // Routes
 // =============================================================
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "HOMEPAGE"));
+  res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get('*', function(req, res) {
-    res.redirect('/');
-});
+// app.get('*', function(req, res) {
+//     res.redirect('/');
+// });
 
 app.get("/reserve", function(req, res) {
-  res.sendFile(path.join(__dirname, "RESERVATION PAGE"));
+  res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
 app.get("/tables", function(req, res) {
-  res.sendFile(path.join(__dirname, "TABLES PAGE"));
+  res.sendFile(path.join(__dirname, "view.html"));
 });
 
-app.get("/api/:tables?", function(req, res) {
-  var chosen = req.params.tables;
+// app.get("/api/:tables?", function(req, res) {
+//   var chosen = req.params.tables;
 
-  if (chosen) {
-    console.log(chosen);
+//   if (chosen) {
+//     console.log(chosen);
+//     return res.json(reservations);
+    
 
-    for (var i = 0; i < reservations.length; i++) {
-      if (chosen === reservations[i].customerID) {
-        return res.json(reservations[i]);
-      }
-    }
-    for (var i = 0; i < waitlist.length; i++) {
-    	if (chosen === waitlist[i].customerID) {
-    		return res.json(waitlist[i]);
-    	}
-    }
+//     // for (var i = 0; i < reservations.length; i++) {
+//     //   if (chosen === reservations[i].customerID) {
+//     //     return res.json(reservations[i]);
+//     //   }
+//     // }
+//     // for (var i = 0; i < waitlist.length; i++) {
+//     // 	if (chosen === waitlist[i].customerID) {
+//     // 		return res.json(waitlist[i]);
+//     // 	}
+//     // }
+//   }
 
-    return res.json(false);
-  }
-  return res.json(reservations);
-});
+  
+// });
 
 app.get("/api/tables", function(req, res) {
 	res.json(reservations);
 });
 
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/reserve", function(req, res) {
 	res.json(waitlist);
 });
 
